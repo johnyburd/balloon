91dd842bec0fdc047e526fbd873588809efd677c
Jonathan Buchanan <johnyburd8@gmail.com>
2016-04-03 18:38:45 -0400

    updated tester to test

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ import time
 def main():
     print("initializing balloon equipment")
     print("3g... " + cellDongle.status())
-    print("GPS... " + gps.status())
+    print("GPS... ")
+    gps1 = gpsClass("localhost","99")
     print("Webcam 1...")
     cam1 = Webcam("/dev/video1","1600x1200")
     print(cam1.status())
@@ -18,6 +19,13 @@ def main():
 
     print cam1.take_pic()
     print cam2.take_pic()
+
+    print ("lat: "+gps1.getLat())
+    print("lon: "+ gps1.getLon())
+    print("speed: " + gps1.getSpeed())
+    print("alt: " + gps1.getAlt())
+    print("climb: " + gps1.getClimb())
+    print("time: " + gps1.getTime())
 
 
 
