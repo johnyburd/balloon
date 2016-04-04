bd217d364f15595f33903b30faf697efdd4fc2c1
Jonathan Buchanan <johnyburd8@gmail.com>
2016-04-03 21:28:34 -0400

    again

--- barometer.py
+++ barometer.py
@@ -2,16 +2,16 @@ import Adafruit_BMP.BMP085 as BMP085
 
 class barometer:
     def __init___(self):
-        sensor = BMP085.BMP085()
+        self.sensor = BMP085.BMP085()
 
     def getTempC(self):
-        return sensor.read_temperature()
+        return self.sensor.read_temperature()
     def getTempF(self):
-        return (sensor.read_temperature()*9)/5+32
+        return (self.sensor.read_temperature()*9)/5+32
     def getPressure(self):
         #Pa
-        return sensor.read_pressure()
+        return self.sensor.read_pressure()
     def getAlt(self):
-        return sensor.read_altitude()
+        return self.sensor.read_altitude()
     def getSeaLevelPressure(self):
-        return sensor.read_sealevel_pressure()
+        return self.sensor.read_sealevel_pressure()
