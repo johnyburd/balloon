b250675239c625465d969fdd0ec9ff675c4bde5f
Jonathan Buchanan <johnyburd8@gmail.com>
2016-04-05 01:51:54 +0000

    bugfixes and trials and errors

--- gpsClass.py
+++ gpsClass.py
@@ -2,7 +2,8 @@ import gps
 
 class GpsClass:
     def __init__(self, host, port):
-        self.session = gps.gps(host,port)
+        #self.session = gps.gps(host,port)
+        self.session = gps.gps("localhost","99")
         self.session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
     def report(self, reportType):
@@ -10,28 +11,10 @@ class GpsClass:
             try:
                 report = self.session.next()
                 if report['class'] == 'TPV':
-                    if hasattr(report, reportType):
-                        return report.reportType
+                    if hasattr(report, 'speed'):
+                        return report.speed
             except:
                 return 1
-
-    def getDMS(self):
-        lt = getLat()
-        ln = getLon()
-
-        lat = decToDMS(lt)
-        lon = decToDMS(ln)
-
-        latHem = 'N'
-        lonHem = 'E'
-
-        if !lat[0]:
-            latHem = 'S'
-        if !lon[0]:
-            lonHem = 'W'
-
-        return lat[1] + "d" + lat[2] + "'{0:0.3f}\"".format(lat[3]) + latHem + " " + lon[1] + "d" + lon[2] + "'{0:0.3f}\"".format(lon[3]) + lonHem
-
 
     def decToDMS(dec):
         pos = True
@@ -56,7 +39,7 @@ class GpsClass:
         return self.report("lon")
 
     def getSpeed(self):
-        return self.report("speed")
+        return self.report('speed')
 
     def getAlt(self):
         return self.report("alt")
@@ -66,3 +49,22 @@ class GpsClass:
 
     def getTime(self):
         return self.report("time")
+
+    def getDMS(self):
+        lt = self.getLat()
+        ln = self.getLon()
+
+        lat = self.decToDMS(lt)
+        lon = self.decToDMS(ln)
+
+        latHem = 'N'
+        lonHem = 'E'
+
+        if not lat[0]:
+            latHem = 'S'
+        if not lon[0]:
+            lonHem = 'W'
+
+        return lat[1] + "d" + lat[2] + "'{0:0.3f}\"".format(lat[3]) + latHem + " " + lon[1] + "d" + lon[2] + "'{0:0.3f}\"".format(lon[3]) + lonHem
+
+
